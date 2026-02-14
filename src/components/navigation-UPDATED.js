921c53e048a70ddbe9200eaf42e2822ff73876cf
tellemarkel-del <tellemarkel@gmail.com>
2026-02-15 00:29:42 +0100

    Add files via upload

---
+++ src/components/navigation-UPDATED.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import './navigation.css'
+
+const Navigation = (props) => {
+  return (
+    <nav className={`navigation-navigation ${props.rootClassName} `}>
+      <Link to="/" className="navigation-logo">
+        PPC Atlas
+      </Link>
+      <div className="navigation-nav">
+        <Link to="/google-ads-hub" className="navigation-link">
+          Google Ads
+        </Link>
+        <Link to="/meta-ads-hub" className="navigation-link">
+          Meta Ads
+        </Link>
+        <a href="#contact" className="navigation-contact-button">
+          Contact
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+Navigation.defaultProps = {
+  rootClassName: '',
+}
+
+Navigation.propTypes = {
+  rootClassName: PropTypes.string,
+}
+
+export default Navigation
